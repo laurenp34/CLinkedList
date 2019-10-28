13a5d8eef89338e315ffc0f5ebb1db88832f6981
Lauren <laurenpehlivanian@gmail.com>
2019-10-28 02:53:47 +0100

    edited headers

--- node.h
+++ node.h
@@ -8,11 +8,12 @@ struct node {
   struct node * next;
 };
 
-void print_list(struct node *);
+
+void print_list(struct node * myNode);
 
     // Should take a pointer to a node struct and print out all of the data in the list
 
-//struct node * insert_front(struct node *, int);
+struct node * insert_front(struct node * myNode, int val);
 
     // Should take a pointer to the existing list and the data to be added, create a new node and put it at the beginning of the list.
     // The second argument should match whatever data you contain in your nodes.
